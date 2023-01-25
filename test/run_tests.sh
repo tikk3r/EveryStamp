2489aeaebcd1f8c35051189bd311ba6e3a1090aa
Frits Sweijen <tikk3r@users.noreply.github.com>
2023-01-25 17:30:58 +0100

    Update CI

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,3 +1,4 @@
+rm -rf tests_output
 mkdir tests_output
 echo Testing Legacy download
 everystamp --survey legacy --ra 165.345 --dec 65.567 --size 0.1 --mode jpeg --legacy_bands grz --legacy_layer ls-dr9 --legacy_autoscale --download_dir tests_output
