112d73bfbc0048afb72b3ada26c9b1fe16a09c16
jurjen93 <jurjendejong93@gmail.com>
2024-01-29 21:19:43 +0100

    reformat and dependencies

--- src/everystamp/cutters.py
+++ src/everystamp/cutters.py
@@ -1,16 +1,21 @@
 """Sub-module for trimming FITS images."""
 
+import sys
+from typing import Union
+
+import pyregion
 from astropy.io import fits
 from astropy.nddata import Cutout2D
 from astropy.wcs import WCS
-import pyregion
 from numpy import array
-from typing import Union
-import sys
 
 
-def make_cutout_2D(image: str = None, pos: Union[list, tuple] = None,
-                   size: Union[list, tuple] = None, outfile: str = None):
+def make_cutout_2D(
+    image: str = None,
+    pos: Union[list, tuple] = None,
+    size: Union[list, tuple] = None,
+    outfile: str = None,
+):
     """
     Make 2D cutout with astropy
     ---------------------------
@@ -51,26 +56,30 @@ def make_cutout_region(image: str = None, region: str = None, outfile: str = Non
     r = pyregion.open(region).as_imagecoord(header=head)
     mask = r.get_mask(hdu=hdu[0], shape=(head["NAXIS1"], head["NAXIS2"]))
 
-    assert len(r) == 1, 'Multiple regions in 1 file given, only one allowed'
+    assert len(r) == 1, "Multiple regions in 1 file given, only one allowed"
 
     shape = array(r[0].coord_list)
 
     # circle
     if len(shape) == 3:
-        cutout = Cutout2D(data=data * mask,
-                          position=(shape[0], shape[1]),
-                          size=(shape[2], shape[2]),
-                          wcs=WCS(head, naxis=2),
-                          mode='partial')
+        cutout = Cutout2D(
+            data=data * mask,
+            position=(shape[0], shape[1]),
+            size=(shape[2], shape[2]),
+            wcs=WCS(head, naxis=2),
+            mode="partial",
+        )
     # square
     elif len(shape) > 3:
         print(shape)
 
-        cutout = Cutout2D(data=data * mask,
-                          position=(shape[0], shape[1]),
-                          size=(shape[3], shape[2]),
-                          wcs=WCS(head, naxis=2),
-                          mode='partial')
+        cutout = Cutout2D(
+            data=data * mask,
+            position=(shape[0], shape[1]),
+            size=(shape[3], shape[2]),
+            wcs=WCS(head, naxis=2),
+            mode="partial",
+        )
     else:
         sys.exit("ERROR: Should not arrive here")
 
